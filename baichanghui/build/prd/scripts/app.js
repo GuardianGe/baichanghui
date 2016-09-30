08cc7ff411ec41f146e8aee26fde0b5d3b23503e
DBliuchengjie <812335929@qq.com>
2016-09-30 16:56:00 +0800

    2016.9.30框架搭建完成，并上传

---
+++ baichanghui/build/prd/scripts/app.js
@@ -0,0 +1,106 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(1);
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/*** IMPORTS FROM imports-loader ***/
+	var define = false;
+
+	__webpack_require__(2);
+
+
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/*** IMPORTS FROM imports-loader ***/
+	var define = false;
+
+
+	var str = __webpack_require__(3);
+
+	var common = __webpack_require__(4);
+
+	common.append($('body'), str);
+
+
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	module.exports = "<div>    123344</div>"
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	/*** IMPORTS FROM imports-loader ***/
+	var define = false;
+
+	var common = {
+	    renderBody: function ($el, str) {
+	        $el.prepend(str);
+	  },
+	    inner: function ($el, str) {
+	        $el.html(str);
+	  },
+	    append: function ($el, str) {
+	        $el.append(str);
+	  }
+	}
+	module.exports = common;
+
+
+
+/***/ }
+/******/ ]);
---
+++ baichanghui/src/scripts/views/index.js
@@ -0,0 +1,6 @@
+
+var str = require('../tpls/index.string');
+
+var common = require('../utils/common.util.js');
+
+common.append($('body'), str);
